5007cabc7bd9672a905ddbefe6af01b71ad796b4
甘杨 <GaryYang@bayview-tech.com>
2016-11-15 17:23:59 +0800

    更换图标

--- src/app/index.constants.js
+++ src/app/index.constants.js
@@ -7,8 +7,11 @@
         .constant('moment', moment)
         .constant('lodash', _)
         .constant('JsBarcode', JsBarcode)
-        .constant('appUrl', 'https://api.vi-ni.com/app/v1/')
-        .constant('restUrl', 'https://api.vi-ni.com/webapi/v1/')
-        .constant('restUrlV2', 'https://api.vi-ni.com/webapi/v2/');
+        .constant('appUrl', 'http://localhost:8090/app/')
+        .constant('restUrl', 'http://localhost:8090/rest/')
+        .constant('restUrlV2', 'http://localhost:8090/rest2/');
+        // .constant('appUrl', 'https://api.vi-ni.com/app/v1/')
+        // .constant('restUrl', 'https://api.vi-ni.com/webapi/v1/')
+        // .constant('restUrlV2', 'https://api.vi-ni.com/webapi/v2/');
 
 })();
